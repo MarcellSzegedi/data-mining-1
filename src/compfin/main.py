04007f6995db533c31216f4423691806f59b6fee
MarcellSzegedi <marcellvonszeged@gmail.com>
2025-04-03 16:30:40 +0200

    Further configuration of the repo with ruff: (linting + fomratting)

---
+++ src/compfin/main.py
@@ -0,0 +1,10 @@
+"""Main module."""
+
+
+def main():
+    """Example main function. Will be changed later."""
+    print("Hello from data-mining!")
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,6 @@
+# ruff: noqa: D100, D101, D102, D103
 import unittest
+
 
 class TestExample(unittest.TestCase):
     def test_example(self):
